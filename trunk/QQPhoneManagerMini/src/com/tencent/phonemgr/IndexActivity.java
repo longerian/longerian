dc724ffdd8ede51cad3d676bde41b9300ac71956
xlhongultimate@gmail.com <xlhongultimate@gmail.com@86ccf4b3-47ce-4ab3-0786-7d483d0d4edd>
2012-08-30 08:33:51 +0000

    添加字符串

--- trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/IndexActivity.java
+++ trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/IndexActivity.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class IndexActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.activity_index, menu);
         return true;
     }
 }
