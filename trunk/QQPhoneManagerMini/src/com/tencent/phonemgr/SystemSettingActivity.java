36e5ea521c29b8cb1809cd00b9e2f73124dae852
xlhongultimate@gmail.com <xlhongultimate@gmail.com@86ccf4b3-47ce-4ab3-0786-7d483d0d4edd>
2012-08-31 06:55:53 +0000

    完成wifi设置

--- trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/SystemSettingActivity.java
+++ trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/SystemSettingActivity.java
@@ -1,24 +1,33 @@
 package com.tencent.phonemgr;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.KeyEvent;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class SystemSettingActivity extends SherlockPreferenceActivity {
 	
+	private String keyWifi;
+	private String keyBluetooth; 
+	private String keyMobileNetwork;
 	private String keySilent;
 	private String keyAutoRotate;
+	
+	private CheckBoxPreference mWifiPreference;
+	private CheckBoxPreference mBluetoothPreference;
+	private CheckBoxPreference mMobileNetworkPreference;
 	private CheckBoxPreference mSilentPreference;
 	private CheckBoxPreference mAutoRotatePreference;
 	
@@ -27,8 +36,21 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		keyWifi = getString(R.string.key_wifi);
+		keyBluetooth = getString(R.string.key_bluetooth);
+		keyMobileNetwork = getString(R.string.key_mobile_network);
 		keySilent = getString(R.string.key_silent);
 		keyAutoRotate = getString(R.string.key_auto_rotate);
+		
+		mWifiPreference = (CheckBoxPreference) findPreference(keyWifi);
+		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		mWifiPreference.setChecked(wm.isWifiEnabled());
+		
+		mBluetoothPreference = (CheckBoxPreference) findPreference(keyBluetooth);
+		mBluetoothPreference.setChecked(BluetoothAdapter.getDefaultAdapter().isEnabled());
+		
+		mMobileNetworkPreference = (CheckBoxPreference) findPreference(keyMobileNetwork);
 		
 		mSilentPreference = (CheckBoxPreference) findPreference(keySilent);
 		AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
@@ -41,11 +63,11 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		IntentFilter filter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
-		registerReceiver(mAudioModeListener, filter);
+		registerReceiver(mAudioModeChangedReceiver, new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION));
+		registerReceiver(mWifiStateChangedReceiver, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
 	}
 	
-	private BroadcastReceiver mAudioModeListener = new BroadcastReceiver() {
+	private BroadcastReceiver mAudioModeChangedReceiver = new BroadcastReceiver() {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -55,10 +77,20 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 		
 	};
 	
+	private BroadcastReceiver mWifiStateChangedReceiver = new BroadcastReceiver() {
+
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+	        WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+	        mWifiPreference.setChecked(wm.isWifiEnabled());
+	    }
+	};
+	
 	@Override
 	protected void onStop() {
 		super.onStop();
-		unregisterReceiver(mAudioModeListener);
+		unregisterReceiver(mAudioModeChangedReceiver);
+		unregisterReceiver(mWifiStateChangedReceiver);
 	}
 
 	@Override
@@ -66,7 +98,16 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
 			Preference preference) {
 		String key = preference.getKey();
-		if(key.equals(keySilent)) {
+		if(key.equals(keyWifi)) {
+			WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+			wm.setWifiEnabled(!wm.isWifiEnabled());
+		} else if(key.equals(keyBluetooth)) {
+			if(BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+				BluetoothAdapter.getDefaultAdapter().disable();
+			} else {
+				BluetoothAdapter.getDefaultAdapter().enable();
+			}
+		} else if(key.equals(keySilent)) {
 			AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 			switch(am.getRingerMode()) {
 			case AudioManager.RINGER_MODE_SILENT:
@@ -80,7 +121,6 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 			int flag = Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0);
 			Settings.System.putInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, flag == 1 ? 0 : 1);
 		}
-		
 		return true;
 	}
 
