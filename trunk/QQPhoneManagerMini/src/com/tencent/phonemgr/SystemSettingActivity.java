9ce950e42296cbbbbce4267a499982f00ad2a180
xlhongultimate@gmail.com <xlhongultimate@gmail.com@86ccf4b3-47ce-4ab3-0786-7d483d0d4edd>
2012-08-31 07:36:09 +0000

    基本完成网络数据、飞行模式的设置

--- trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/SystemSettingActivity.java
+++ trunk/QQPhoneManagerMini/src/com/tencent/phonemgr/SystemSettingActivity.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -19,15 +17,17 @@ import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class SystemSettingActivity extends SherlockPreferenceActivity {
 	
+	private String keyAirplaneMode;
 	private String keyWifi;
 	private String keyBluetooth; 
 	private String keyMobileNetwork;
 	private String keySilent;
 	private String keyAutoRotate;
 	
+	private CheckBoxPreference mAirPlaneModePreference;
 	private CheckBoxPreference mWifiPreference;
 	private CheckBoxPreference mBluetoothPreference;
-	private CheckBoxPreference mMobileNetworkPreference;
+	private Preference mMobileNetworkPreference;
 	private CheckBoxPreference mSilentPreference;
 	private CheckBoxPreference mAutoRotatePreference;
 	
@@ -37,20 +37,25 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
 		
+		keyAirplaneMode = getString(R.string.key_airplane_mode);
 		keyWifi = getString(R.string.key_wifi);
 		keyBluetooth = getString(R.string.key_bluetooth);
 		keyMobileNetwork = getString(R.string.key_mobile_network);
 		keySilent = getString(R.string.key_silent);
 		keyAutoRotate = getString(R.string.key_auto_rotate);
 		
+		mAirPlaneModePreference = (CheckBoxPreference) findPreference(keyAirplaneMode);
+		mAirPlaneModePreference.setChecked(Settings.System.getInt(getContentResolver(),	Settings.System.AIRPLANE_MODE_ON, 0) == 1);
+		
 		mWifiPreference = (CheckBoxPreference) findPreference(keyWifi);
 		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 		mWifiPreference.setChecked(wm.isWifiEnabled());
 		
+		//TODO listen the bluetooth state
 		mBluetoothPreference = (CheckBoxPreference) findPreference(keyBluetooth);
 		mBluetoothPreference.setChecked(BluetoothAdapter.getDefaultAdapter().isEnabled());
 		
-		mMobileNetworkPreference = (CheckBoxPreference) findPreference(keyMobileNetwork);
+		mMobileNetworkPreference = findPreference(keyMobileNetwork);
 		
 		mSilentPreference = (CheckBoxPreference) findPreference(keySilent);
 		AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
@@ -98,30 +103,70 @@ public class SystemSettingActivity extends SherlockPreferenceActivity {
 	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
 			Preference preference) {
 		String key = preference.getKey();
-		if(key.equals(keyWifi)) {
-			WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-			wm.setWifiEnabled(!wm.isWifiEnabled());
+		if(key.equals(keyAirplaneMode)) {
+			toggleAirplaneMode();
+		} else if(key.equals(keyWifi)) {
+			toggleWifi();
 		} else if(key.equals(keyBluetooth)) {
-			if(BluetoothAdapter.getDefaultAdapter().isEnabled()) {
-				BluetoothAdapter.getDefaultAdapter().disable();
-			} else {
-				BluetoothAdapter.getDefaultAdapter().enable();
-			}
+			toggleBluetooth();
+		} else if(key.equals(keyMobileNetwork)) {
+			toggleMobileNetwork();
 		} else if(key.equals(keySilent)) {
-			AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-			switch(am.getRingerMode()) {
-			case AudioManager.RINGER_MODE_SILENT:
-		        am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-		        break;
-		    default:
-		    	am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-		    	break;
-			}
+			toggleRingerMode();
 		} else if(key.equals(keyAutoRotate)) {
-			int flag = Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0);
-			Settings.System.putInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, flag == 1 ? 0 : 1);
+			toggleAutoRotate();
 		}
 		return true;
 	}
+	
+	private void toggleAirplaneMode() {
+		boolean isEnabled = Settings.System.getInt(
+				getContentResolver(),
+				Settings.System.AIRPLANE_MODE_ON, 0) == 1;
 
+		Settings.System.putInt(
+		      getContentResolver(),
+		      Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
+
+		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+		intent.putExtra("state", !isEnabled);
+		sendBroadcast(intent);
+	}
+	
+	private void toggleWifi() {
+		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		wm.setWifiEnabled(!wm.isWifiEnabled());
+	}
+	
+	private void toggleBluetooth() {
+		if(BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+			BluetoothAdapter.getDefaultAdapter().disable();
+		} else {
+			BluetoothAdapter.getDefaultAdapter().enable();
+		}
+	}
+	
+	private void toggleMobileNetwork() {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setClassName("com.android.phone", "com.android.phone.Settings");
+		startActivity(intent);
+	}
+	
+	private void toggleRingerMode() {
+		AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		switch(am.getRingerMode()) {
+		case AudioManager.RINGER_MODE_SILENT:
+	        am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+	        break;
+	    default:
+	    	am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+	    	break;
+		}
+	}
+	
+	private void toggleAutoRotate() {
+		int flag = Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0);
+		Settings.System.putInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, flag == 1 ? 0 : 1);
+	}
+	
 }
